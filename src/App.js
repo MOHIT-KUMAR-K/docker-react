8bc21211da9f5a0a9280cadc59ed3752627a5b68
Mohit <mohitkumarkatragadda@gmail.com>
2020-07-05 15:42:30 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello and welcome to Docker
         </p>
         <a
           className="App-link"
